a66e175ba11d9b33801a0acef30dc5071ac3c592
筱彤 赵 <275451183@qq.com>
2020-08-25 11:50:21 +0800

    Change file CreateCodingTestDB.sql

--- CreateDBScript/CreateCodingTestDB.sql
+++ CreateDBScript/CreateCodingTestDB.sql
@@ -1,4 +1,23 @@
 CREATE DATABASE IF NOT EXISTS CodingTest;
 USE CodingTest;
-CREATE TABLE IF NOT EXISTS  TblLeft (Id int(11) auto_increment primary key, ReposName longtext, CloneUrl longtext, Language longtext, Description longtext, CreatedOn DateTime(6), CreatedBy longtext, ModifiedOn datetime(6), ModifiedBy longtext);
-CREATE TABLE IF NOT EXISTS  TblRight (Id int(11) auto_increment primary key, TblLeftId int(11), ReposName longtext, CloneUrl longtext, Language longtext, Description longtext, IsDeleted boolean, DeletedDescription longtext, CreatedOn DateTime(6), CreatedBy longtext, ModifiedOn datetime(6), ModifiedBy longtext);
+CREATE TABLE IF NOT EXISTS  TblLeft (
+Id int(11) auto_increment primary key, 
+TblReposDetailId int(11),
+CreatedOn DateTime(6));
+
+CREATE TABLE IF NOT EXISTS  TblRight (
+Id int(11) auto_increment primary key, 
+TblReposDetailId int(11), 
+IsDeleted boolean, 
+DeletedDescription longtext, 
+CreatedOn DateTime(6));
+
+CREATE TABLE IF NOT EXISTS  TblReposDetail (
+Id int(11) auto_increment primary key, 
+ReposId longtext,
+ReposName longtext,
+CloneUrl longtext, 
+Language longtext, 
+Description longtext,
+CreatedOn DateTime(6),
+ModifiedOn DateTime(6));
